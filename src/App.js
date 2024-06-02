a8a8125172197cecef38f6d4ef3f76859c68798c
knford01 <54276167+knford01@users.noreply.github.com>
2024-06-02 09:37:21 -0500

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css";
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import Employee from "./components/Employee";
+// import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
+export default function App() {
+  const [role, setRole] = useState("Not Assigned");
+  const [employees, setEmployees] = useState([
+    {
+      name: "Icabod",
+      role: "Dingleberry",
+      img: "/images/male smiling.jpg",
+    },
+    {
+      name: "Jezabel",
+      role: "Trouble Maker",
+      img: "/images/curlyfemale.png",
+    },
+    {
+      name: "Liclomas",
+      role: "King of Poopies",
+      img: "/images/male clean.jpg",
+    },
+    {
+      name: "Fruggzie",
+      role: "Stone Cold Killer",
+      img: "/images/male bearded.jpg",
+    },
+    {
+      name: "Margomadison",
+      role: "SCAB",
+      img: "/images/female fancy.jpg",
+    },
+    {
+      name: "Baby Jesus",
+      role: "Just a Baby",
+      img: "/images/Minniecat.png",
+    },
+  ]);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input
+          type="text"
+          onChange={(e) => {
+            console.log(e.target.value);
+            setRole(e.target.value);
+          }}
+        />
+        <div className="flex flex-wrap justify-center">
+          {employees.map((employee) => {
+            return (
+              <Employee
+                key={uuidv4()}
+                name={employee.name}
+                role={employee.role}
+                img={employee.img}
+              />
+            );
+          })}
+        </div>
       </header>
     </div>
   );
 }
-
-export default App;
