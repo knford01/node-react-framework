90ca1f0084bd41a3002afe7d05ca096ad5de231e
knford01 <54276167+knford01@users.noreply.github.com>
2024-06-04 15:35:37 -0500

    Form and Button Styling

--- src/pages/Customer.js
+++ src/pages/Customer.js
@@ -6,9 +6,20 @@ import { baseUrl } from '../shared';
 export default function Customer() {
     const { id } = useParams();
     const [customer, setCustomer] = useState();
+    const [tempCustomer, setTempCustomer] = useState();
     const [notFound, setNotFound] = useState(false);
+    const [changed, setChanged] = useState(false);
+    const [error, setError] = useState();
+
     const navigate = useNavigate();
 
+    useEffect(() => {
+        // console.log('customer', customer);
+        // console.log('temp customer', tempCustomer);
+        compareObjects();
+    })
+
+    //This is where you will initally get the customer information\\
     useEffect(() => {
         const url = baseUrl + 'api/customers/' + id;
 
@@ -20,25 +31,79 @@ export default function Customer() {
                     // navigate('/404');
                     //render 404 componenet\\
                     setNotFound(true);
+                } else {
+                    return response.json();
                 }
-                return response.json();
             })
             .then((data) => {
-                setCustomer(data.customer);
+                if (data !== undefined) {
+                    setCustomer(data.customer);
+                    setTempCustomer(data.customer);
+                } else {
+                    setNotFound(true);
+                }
             })
     }, [id]);
 
-    const error = 'Customer with id ' + id + ' was not found';
+    function updateCustomer() {
+        const url = baseUrl + 'api/customers/' + id;
+        fetch(url, { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(tempCustomer) })
+            .then((response) => { if (!response.ok) throw new Error('Failed to update customer'); return response.json(); })
+            .then((data) => { setChanged(false); setCustomer(data.customer); setError(undefined); console.log(data); })
+            .catch((e) => {
+                // console.log(e);
+                setError(e.message);
+            })
+    }
 
+    function compareObjects() {
+        if (JSON.stringify(customer) === JSON.stringify(tempCustomer)) {
+            setChanged(false);
+        }
+    }
+
+    const error_msg = 'Customer with id ' + id + ' was not found';
     return (
         <>
-            {notFound ? <NotFound error={error} /> : null}
+            {notFound ? <NotFound error={error_msg} /> : null}
             {customer ? (
                 <div>
-                    <p>{customer.id}</p>
-                    <p>{customer.name}</p>
+                    {/* ID: <input className="m-2 block px-2" type="text" value={tempCustomer.id} /> */}
+                    Name: <input className="m-2 block px-2" type="text" value={tempCustomer.name} onChange={(e) => { setChanged(true); setTempCustomer({ ...tempCustomer, name: e.target.value }) }} />
+                    Contact: <input className="m-2 block px-2" type="text" value={tempCustomer.contact} onChange={(e) => { setChanged(true); setTempCustomer({ ...tempCustomer, contact: e.target.value }) }} />
+                    {changed ?
+                        <>
+                            <button className="mx-2" onClick={() => { setTempCustomer({ ...customer }); setChanged(false) }}>
+                                Cancel
+                            </button>
+                            <button className="mx-2" onClick={updateCustomer}>
+                                Save
+                            </button></>
+                        : null
+                    }
+
+                    <button onClick={(e) => {
+                        const url = baseUrl + 'api/customers/' + id;
+
+                        fetch(url, { method: 'DELETE', headers: { 'Content-Type': 'application/json' } })
+                            .then((response) => {
+                                if (!response.ok) {
+                                    throw new Error('Something went wrong');
+                                }
+                                navigate('/customers');
+                            })
+                            .catch((e) => {
+                                setError(e.message);
+                                // console.log(e);
+                            })
+                    }}>Delete</button >
                 </div>
             ) : null}
+
+            {error ? <p>{error}</p> : null}
+
+            <br />
+
             <Link to='/customers'>Return to Customers</Link>
         </>
     )
--- src/pages/Customers.js
+++ src/pages/Customers.js
@@ -2,9 +2,16 @@ import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom';
 import { v4 as uuidv4 } from "uuid";
 import { baseUrl } from '../shared';
+import AddCustomer from '../components/AddCustomer';
 
 function Customers() {
     const [customers, setCustomers] = useState();
+    const [show, setShow] = useState(false);
+
+    function toggleShow() {
+        setShow(!show);
+    }
+
     useEffect(() => {
         // console.log('Fetching Customers...');
         const url = baseUrl + 'api/customers/';
@@ -15,14 +22,40 @@ function Customers() {
                 setCustomers(data.customers);
             })
     }, []);
+
+    function newCustomer(name, contact) {
+        const data = { name: name, contact: contact }
+        const url = baseUrl + 'api/customers/';
+        fetch(url, { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(data) })
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error('Something went wrong');
+                }
+                return response.json();
+            })
+            .then((data) => {
+                // console.log(data);
+                // setCustomers(data.customers);
+                toggleShow();
+                setCustomers([...customers, data.customer]);
+            })
+            .catch((e) => {
+                console.log(e);
+            })
+    }
+
     return (
         <>
             Here are our customers:
-            {customers ?
-                customers.map((customer) => {
-                    return <p key={uuidv4()}><Link to={"/customers/" + customer.id}>{customer.name}</Link></p>;
-                })
-                : null}
+            <ul>
+                {customers ?
+                    customers.map((customer) => {
+                        return <li key={uuidv4()}><Link to={"/customers/" + customer.id}>{customer.name}</Link></li>;
+                    })
+                    : null
+                }
+            </ul>
+            <AddCustomer newCustomer={newCustomer} show={show} toggleShow={toggleShow} />
         </>
     )
 }
